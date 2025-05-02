7d2cf1d4db4963432315ab0130274c7b09aea3a1
MengChen <tatsukimengchen@163.com>
2025-05-02 16:54:16 +0800

    refactor: 移除控制器版本控制配置，简化 AppController

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ import { AllExceptionsFilter } from './common/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
